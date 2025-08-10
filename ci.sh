aa7dcd77ee2a75c6f9a4c2c152133b965ec800ef
Steve O'Brien <steve@obrien.cc>
2025-08-10 15:45:45 -0400

    Add CI

--- ci.sh
+++ ci.sh
@@ -2,27 +2,49 @@
 
 # Run `./ci.sh` from within project root dir
 
+CLANG=${CLANG:-clang++}
+
 TESTCC=test/test_binspect.cc
-BUILD=build/
+BUILD=$(mktemp -d)
 
 set -xeuo pipefail
 
 rm -rf build
 mkdir build
 
-clang++                         \
-  @compile_flags.txt            \
-  -O0 -g                        \
-  -o "${BUILD}/test"            \
-  -fsanitize=address,undefined  \
-  -UNDEBUG                      \
-  "${TESTCC}"
+for OPT in O0 O1 Os Og O3 ; do
+  for SAN in undefined address thread ; do
+    PROG="${BUILD}/test_${OPT}_${SAN}"
+    rm -f "${PROG}"
+    "${CLANG}"                      \
+      @compile_flags.txt            \
+      -g                            \
+      "-${OPT}"                     \
+      -o "${PROG}"                  \
+      -fsanitize="${SAN}"           \
+      -UNDEBUG                      \
+      "${TESTCC}"
 
-"${BUILD}/test" test/files/elf.64.le.exe
-"${BUILD}/test" test/files/elf.64.le.so
+    for FORMAT in elf ; do
+      for BITS in 64 32 ; do
+        for ENDIAN in le be ; do
+          for TYPE in exe so ; do
+            # Don't currently have big-endian shared libs
+            if [[ "${FORMAT}-${ENDIAN}-${TYPE}" == "elf-be-so" ]]; then continue; fi
+            BINFILE="test/files/${FORMAT}.${BITS}.${ENDIAN}.${TYPE}"
+            "${PROG}" "${BINFILE}"
+          done
+        done
+      done
+    done
+  done
+done
 
-"${BUILD}/test" test/files/elf.32.le.exe
-"${BUILD}/test" test/files/elf.32.le.so
+# "${BUILD}/test" test/files/elf.64.le.exe
+# "${BUILD}/test" test/files/elf.64.le.so
 
-"${BUILD}/test" test/files/elf.64.be.exe
-"${BUILD}/test" test/files/elf.32.be.exe
+# "${BUILD}/test" test/files/elf.32.le.exe
+# "${BUILD}/test" test/files/elf.32.le.so
+
+# "${BUILD}/test" test/files/elf.64.be.exe
+# "${BUILD}/test" test/files/elf.32.be.exe
